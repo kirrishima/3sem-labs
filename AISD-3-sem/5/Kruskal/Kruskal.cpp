af3075c58659ee1122765853c672503efbc9f15f
Force <maxfunmaxfun672@gmail.com>
2024-10-16 00:01:54 +0300

    a

--- AISD-3-sem/5/Kruskal/Kruskal.cpp
+++ AISD-3-sem/5/Kruskal/Kruskal.cpp
@@ -12,44 +12,40 @@ struct Edge {
 	}
 };
 
-struct UnionFind {
-	vector<int> parent;
+vector<int> parent;
 
-	UnionFind(int n) {
-		parent.resize(n);
-		for (int i = 0; i < n; ++i)
-			parent[i] = i;
+int find(int u) {
+	while (u != parent[u])
+	{
+		u = parent[u];
 	}
+	return parent[u];
+}
 
-	int find(int u) {
-		while (u != parent[u])
-		{
-			u = parent[u];
-		}
-		return parent[u];
+void unite(int u, int v) {
+	int root_u = find(u);
+	int root_v = find(v);
+
+	if (root_u != root_v) {
+		parent[root_u] = root_v;
 	}
+}
 
-	void unite(int u, int v) {
-		int root_u = find(u);
-		int root_v = find(v);
 
-		if (root_u != root_v) {
-			parent[root_u] = root_v;
-		}
-	}
-};
-
-vector<Edge> kruskal(int n, vector<Edge>& edges) {
+vector<Edge> kruskal(vector<Edge>& edges, int n) {
 	vector<Edge> mst;
 
 	sort(edges.begin(), edges.end());
 
-	UnionFind uf(n);
+	parent.resize(n);
+	for (int i = 0; i < n; ++i) {
+		parent[i] = i;
+	}
 
 	for (const auto& edge : edges) {
-		if (uf.find(edge.u) != uf.find(edge.v)) {
+		if (find(edge.u) != find(edge.v)) {
 			mst.push_back(edge);
-			uf.unite(edge.u, edge.v);
+			unite(edge.u, edge.v);
 		}
 	}
 
@@ -58,7 +54,7 @@ vector<Edge> kruskal(int n, vector<Edge>& edges) {
 
 int main() {
 	setlocale(LC_ALL, "ru");
-	int n = 8;
+
 	vector<Edge> edges = {
 		{0, 1, 2},
 		{0, 3, 8},
@@ -78,14 +74,11 @@ int main() {
 		{6, 7, 9},
 	};
 
-	vector<Edge> mst = kruskal(n, edges);
+	vector<Edge> mst = kruskal(edges, 8);
 
-	int minWeight = 0;
 	cout << "Минимальное остовное дерево содержит рёбра:\n";
 	for (const auto& edge : mst) {
 		cout << "Ребро: " << edge.u + 1 << " - " << edge.v + 1 << " с весом " << edge.weight << endl;
-		minWeight += edge.weight;
 	}
-	cout << "Минимальная стоимость пути: " << minWeight;
 	return 0;
 }
