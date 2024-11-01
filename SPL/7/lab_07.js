d6bdd114174a63ef35d9b59f87ef7df0071bcb06
Force <maxfunmaxfun672@gmail.com>
2024-11-01 15:58:26 +0300

    update files via auto commit

---
+++ SPL/7/lab_07.js
@@ -0,0 +1,33 @@
+///////////////// Задание 1 ///////////////
+console.log('\n==== Задание 1 ====\n')
+
+let person = {
+    name: "Oleg",
+    age: 52,
+    greet: function () { console.log(`Привет, ${this.name}!`); },
+    ageAfterYears: function (years) { return this.age + years; }
+}
+
+person.greet();
+console.log(person.ageAfterYears(18));
+
+///////////////// Задание 2 ///////////////
+console.log('\n==== Задание 2 ====\n')
+
+let car = {
+    model: "Tesla",
+    year: 2077,
+    getInfo: function () { console.log(`Модель: ${this.model}, Year: ${this.year}`) }
+}
+
+car.getInfo();
+///////////////// Задание 3 ///////////////
+console.log('\n==== Задание 3 ====\n')
+
+
+
+///////////////// Задание 4 ///////////////
+console.log('\n==== Задание 4 ====\n')
+
+
+
