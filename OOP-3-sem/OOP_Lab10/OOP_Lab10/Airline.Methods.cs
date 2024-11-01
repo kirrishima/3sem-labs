71d080c8bafcbe92c4d79cacb19ba494d1b30b2d
Force <maxfunmaxfun672@gmail.com>
2024-11-01 21:47:23 +0300

    update files via auto commit

---
+++ OOP-3-sem/OOP_Lab10/OOP_Lab10/Airline.Methods.cs
@@ -0,0 +1,67 @@
+﻿namespace OOP_Lab02
+{
+    public partial class Airline
+    {
+        static Airline()
+        {
+            if (objectCount == minInt)
+            {
+                objectCount = 0;
+            }
+        }
+
+        private Airline(int id)
+        {
+            this.id = id;
+            objectCount++;
+        }
+
+        public Airline() : this("Minsk, Belarus", 1, "Boeing 777", TimeNow, [DayOfWeek.Monday]) { }
+
+        public Airline(string destination, int flightNumber, string planeType, TimeOnly? departureTime = null,
+                       DayOfWeek[]? daysOfWeeks = null) : this(GenerateId())
+        {
+            Destination = destination ?? throw new ArgumentException("destination is null");
+            FlightNumber = flightNumber;
+            PlaneType = planeType ?? throw new ArgumentException("planeType is null");
+            this.departureTime = departureTime ?? TimeNow;
+            this.daysOfWeeks = daysOfWeeks ?? [DayOfWeek.Sunday];
+        }
+
+        public static void DisplayObjectCount()
+        {
+            Console.WriteLine($"Всего создано бъектов класса Airline: {objectCount}");
+        }
+
+        public static void UpdateFlightNumber(ref int newFlightNumber, out DateTime updatedDepartureTime)
+        {
+            newFlightNumber = 999;
+            updatedDepartureTime = DateTime.Now.AddHours(3);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Airline airline)
+            {
+                return departureTime == airline.departureTime;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(flightNumber, destination);
+        }
+
+        public override string ToString()
+        {
+            return $"Рейс {flightNumber} в {destination}, {planeType}. Отправка: {departureTime.ToString("hh\\:mm")}, Дни: {string.Join(", ", daysOfWeeks)}";
+        }
+
+        private static int GenerateId()
+        {
+            return Guid.NewGuid().GetHashCode();
+        }
+    }
+}
--- OOP-3-sem/OOP_Lab10/OOP_Lab10/Program.cs
+++ OOP-3-sem/OOP_Lab10/OOP_Lab10/Program.cs
@@ -1,19 +1,63 @@
 using System;
+using OOP_Lab02;
 
 namespace OOP_Lab10
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string[] monthes = { "January", "February", "March", "April", "May", "June", "Jully", "August", "September", "October", "November", "December" };
-
-            var a = from m in monthes where m.Length == 7 select m;
-
-            foreach (var item in a)
+            void PrintElements<T>(IEnumerable<T> values)
             {
-                Console.WriteLine(item);
+                Console.WriteLine();
+                foreach (var item in values)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine();
             }
+
+            // 1 задание
+            string[] months = { "December", "January", "February", "March", "April", "May", "September", "October", "November", "June", "July", "August" };
+
+            const int n = 7;
+
+            var a = from m in months where m.Length == n select m;
+
+            PrintElements(a);
+
+            a = months.Take(3).Concat(months.Skip(9));
+            PrintElements(a);
+
+            a = from m in months orderby m select m;
+            PrintElements(a);
+
+            a = from m in months where m.Contains('u') && m.Length > 3 select m;
+            PrintElements(a);
+
+            // 2
+
+            List<Airline> airlines = [
+                new Airline("London, UK", 101, "Airbus A320", new TimeOnly(10, 30, 0), [DayOfWeek.Tuesday, DayOfWeek.Friday]),
+                new Airline("New York, USA", 102, "Boeing 747", new TimeOnly(14, 15, 0), [DayOfWeek.Wednesday, DayOfWeek.Saturday]),
+                new Airline("Tokyo, Japan", 103, "Airbus A380", new TimeOnly(8, 45, 0), [DayOfWeek.Monday, DayOfWeek.Thursday]),
+                new Airline("Paris, France", 104, "Boeing 787", new TimeOnly(16, 0, 0), [DayOfWeek.Sunday, DayOfWeek.Thursday]),
+                new Airline("Berlin, Germany", 105, "Embraer E190", new TimeOnly(12, 30, 0), [DayOfWeek.Monday, DayOfWeek.Friday]),
+                new Airline("Sydney, Australia", 106, "Boeing 777", new TimeOnly(18, 0, 0), [DayOfWeek.Wednesday, DayOfWeek.Saturday]),
+                new Airline("Moscow, Russia", 107, "Sukhoi Superjet 100", new TimeOnly(13, 15, 0), [DayOfWeek.Tuesday, DayOfWeek.Friday]),
+                new Airline("Dubai, UAE", 108, "Airbus A350", new TimeOnly(20, 45, 0), [DayOfWeek.Thursday]),
+                new Airline("Toronto, Canada", 109, "Boeing 737", new TimeOnly(9, 10, 0), [DayOfWeek.Monday, DayOfWeek.Friday]),
+                new Airline("Toronto, Canada", 110, "Boeing 787", new TimeOnly(7, 0, 0), [DayOfWeek.Wednesday, DayOfWeek.Saturday])
+            ];
+
+            // 3
+
+            var dest = airlines.Where((a) => a.Destination.StartsWith("Toronto"));
+
+            PrintElements(dest);
+
+            int c = (from line in airlines where line.DaysOfWeeks.Contains(DayOfWeek.Monday) select line).Count();
+            Console.WriteLine(c);
         }
     }
 }
