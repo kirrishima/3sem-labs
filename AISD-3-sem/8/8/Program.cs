faafef2064107d0bc525652e1fa6bfab85089444
Force <maxfunmaxfun672@gmail.com>
2024-11-28 15:52:47 +0300

    update files via auto commit

--- AISD-3-sem/8/8/Program.cs
+++ AISD-3-sem/8/8/Program.cs
@@ -7,7 +7,7 @@
         public int Cost;
     }
 
-    static int Knapsack(int maxWeight, List<Item> items)
+    static void Knapsack(int maxWeight, List<Item> items)
     {
         int n = items.Count;
 
@@ -28,21 +28,34 @@
                 }
             }
         }
-
         PrintTable(table, maxWeight, items);
-
-        return -1;
     }
 
     static void PrintTable(int[,] table, int maxWeight, List<Item> items)
     {
+        Console.WriteLine("В рюкзаке: ");
+        int cost = 0;
+        for (int i = items.Count; i > 0; i--)
+        {
+            if (table[i - 1, maxWeight] != table[i, maxWeight])
+            {
+                Console.WriteLine($"Название: {items[i - 1].Name}. Масса: {items[i - 1].Weight}. Цена: {items[i - 1].Cost}");
+                maxWeight -= items[i - 1].Weight;
+                cost += items[i - 1].Cost;
+            }
+        }
+        Console.WriteLine($"\nЦена рюкзака: {cost}");
+
+        Console.WriteLine();
+        Console.ReadLine();
+
         Console.Write("    ");
-        for (int w = 0; w <= maxWeight; w++)
+        for (int w = 0; w <= table.GetLength(1) - 1; w++)
         {
             Console.Write($"{w,4}");
         }
         Console.WriteLine();
-        Console.WriteLine(new string('-', (maxWeight + 1) * 4 + 4));
+        Console.WriteLine(new string('-', (table.GetLength(1)) * 4 + 4));
         for (int i = 0; i < table.GetLength(0); i++)
         {
             Console.Write($"{i,2} |");
@@ -53,34 +66,39 @@
             Console.WriteLine();
         }
         Console.WriteLine();
-        Console.WriteLine("В рюкзаке: ");
-        for (int i = items.Count; i > 0; i--)
-        {
-            if (table[i - 1, maxWeight] != table[i, maxWeight])
-            {
-                Console.WriteLine($"Название: {items[i - 1].Name}. Масса: {items[i - 1].Weight}. Цена: {items[i - 1].Cost}");
-                maxWeight -= items[i - 1].Weight;
-            }
-        }
     }
 
     static void Main()
     {
-        var items = new List<Item>() {
-            new Item { Name = "1", Weight = 3, Cost = 2 },
-            new Item { Name = "2", Weight = 1, Cost = 2 },
-            new Item { Name = "3", Weight = 3, Cost = 4 },
-            new Item { Name = "4", Weight = 4, Cost = 5 },
-            new Item { Name = "5", Weight = 2, Cost = 3 }
-        };
-        Console.WriteLine($"Максимальная вместительность: {7}");
-        Console.WriteLine("\nСписок товаров:");
-        foreach (var item in items)
+        Console.Write("Введите количество товаров: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите вместительность рюкзака: ");
+        int N = int.Parse(Console.ReadLine());
+        var items = new List<Item>();
+
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine($"Название: {item.Name}, Вес: {item.Weight}, Стоимость: {item.Cost}");
+            Console.WriteLine();
+            Console.WriteLine($"Введите данные для товара {i + 1}:");
+            Console.Write("Название: ");
+            string name = Console.ReadLine();
+            Console.Write("Вес: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.Write("Стоимость: ");
+            int cost = int.Parse(Console.ReadLine());
+            items.Add(new Item { Name = name, Weight = weight, Cost = cost });
         }
 
-        int maxWeight = 7;
-        var c = Knapsack(maxWeight, items);
+        Console.Clear();
+        Console.WriteLine("Товары: ");
+        foreach (var item in items)
+        {
+            Console.WriteLine($"Название: {item.Name}, Масса: {item.Weight}, Цена: {item.Cost}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Максимальная вместительность: {N}");
+        Console.WriteLine();
+        Knapsack(N, items);
     }
 }
