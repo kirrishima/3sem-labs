34be57f391e8c5eac21c141dbd41e92343a948ec
Force <maxfunmaxfun672@gmail.com>
2024-11-29 13:41:02 +0300

    update files via auto commit

--- SPL/9/lab_09.js
+++ SPL/9/lab_09.js
@@ -6,12 +6,14 @@ let Figure = {
     size: null
 };
 
-let Square = {
-    __proto__: Figure,
+function Square(size) {
+    this.__proto__ = Figure;
+    this.size = size;
 };
 
-let Circle = {
-    __proto__: Figure
+function Circle(color) {
+    this.__proto__ = Figure;
+    this.color = color;
 };
 
 function Triangle(lines) {
@@ -20,20 +22,34 @@ function Triangle(lines) {
 }
 
 function printDiff(obj1, obj2) {
+    let keys1 = Object.keys(obj1).filter(k => !(k in obj2));
+    let keys2 = Object.keys(obj2).filter(k => !(k in obj1));
+
     for (const key in obj1) {
         if (obj1[key] != obj2[key]) {
             console.log(`свойство ${key} отличается (${obj1[key]}, ${obj2[key]})`)
         }
     }
+    console.log(`Уникальные свойства первого объекта: ${keys1}`);
+    console.log(`Уникальные свойства второго объекта: ${keys2}`);
 }
 
 let triangle1 = new Triangle(1);
 let triangle2 = new Triangle(3);
 
-console.log(triangle1);
-console.log(triangle2);
+printDiff(triangle1, triangle2)
+console.log('\n');
 
-printDiff(triangle1,triangle2)
+let circle1 = new Circle("none");
+let circle2 = new Circle("green");
+
+printDiff(circle1, circle2)
+console.log('\n');
+
+let square1 = new Square(10);
+let square2 = new Square(2);
+
+printDiff(square1, square2)
 
 ///////////////// Задание 2 ///////////////
 console.log('\n==== Задание 2 ====\n');
